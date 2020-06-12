0fbdae11ee871dfdadfc9269aa65f1efed1ae520
Victor Kachan <victor.kachan@gmail.com>
2020-06-12 14:05:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const EventEmitter = new NativeEventEmitter(RNAudioRecord);
 
 const AudioRecord = {};
 
-AudioRecord.initRecorder = options => RNAudioRecord.initRecorder(options);
+AudioRecord.init = options => RNAudioRecord.init(options);
 AudioRecord.start = () => RNAudioRecord.start();
 AudioRecord.stop = () => RNAudioRecord.stop();
 AudioRecord.getMaxAmplitude = () => RNAudioRecord.getMaxAmplitude();
-AudioRecord.initPlayer = options => RNAudioRecord.initPlayer(options);
-AudioRecord.pauseMediaPlayer = () => RNAudioRecord.pauseMediaPlayer();
-AudioRecord.playMediaPlayer = () => RNAudioRecord.playMediaPlayer();
-AudioRecord.stopMediaPlayer = () => RNAudioRecord.stopMediaPlayer();
-AudioRecord.getTimeMediaPlayer = () => RNAudioRecord.getTimeMediaPlayer();
-AudioRecord.getDuration = () => RNAudioRecord.getDuration();
 
 const eventsMap = {
   data: 'data'
